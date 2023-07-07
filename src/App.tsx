2eae8218fca768779e2bdd7bfbb8ae56ab995959
v.sauchuk <14895079+olgasavchuk@users.noreply.github.com>
2023-07-07 14:36:02 +0200

    Fix start script node 18 issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,40 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-    );
-  }
+class App extends Component<{}, { monsters: {name: string, id: string}[] }> {
+    constructor() {
+        // @ts-ignore
+        super();
+
+        this.state = {
+            monsters: [],
+        }
+    }
+
+     componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response => response.json())
+          .then(users => {
+              this.setState(
+                () => {
+                    return { monsters: users}
+                },
+                () => {
+                    console.log(this.state)
+                }
+              )
+          })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {
+                    this.state.monsters.map(m => <div><h1 key={m.id}>{m.name}</h1></div>)
+                }
+            </div>
+        );
+    }
 }
 
 export default App;

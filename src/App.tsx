f33b087a576250e18e7203a45f453fbc396a2c0f
v.sauchuk <14895079+olgasavchuk@users.noreply.github.com>
2023-07-07 15:50:30 +0200

    Add search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,66 @@
 import React, {Component} from 'react';
 import './App.css';
 
-class App extends Component<{}, { monsters: {name: string, id: string}[] }> {
-    constructor() {
-        // @ts-ignore
-        super();
+class App extends Component<{}, { monsters: { name: string, id: string }[], searchField: string }> {
+  constructor() {
+    // @ts-ignore
+    super();
 
-        this.state = {
-            monsters: [],
+    this.state = {
+      monsters: [],
+      searchField: '',
+    }
+    console.log('constructor')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
+        this.setState(
+          () => {
+            return {monsters: users}
+          },
+          () => {
+            console.log(this.state)
+          }
+        )
+      })
+  }
+
+  onSearchChange = (event: { target: { value: string; }; }) => {
+    const searchField = event.target.value.toLowerCase()
+    this.setState(
+      () => {
+        return {searchField}
+      },
+      () => {
+        console.log(this.state)
+      }
+    )
+  }
+  render() {
+    console.log('render')
+
+    const { monsters, searchField} = this.state;
+    const { onSearchChange } = this;
+
+    return (
+      <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={ onSearchChange }
+        />
+        {
+          [...monsters.filter(m => m.name.toLowerCase().includes(searchField))]
+            .map(m => <div><h3 key={m.id}>{m.name}</h3></div>)
         }
-    }
-
-     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-          .then(response => response.json())
-          .then(users => {
-              this.setState(
-                () => {
-                    return { monsters: users}
-                },
-                () => {
-                    console.log(this.state)
-                }
-              )
-          })
-    }
-
-    render() {
-        return (
-            <div className="App">
-                {
-                    this.state.monsters.map(m => <div><h1 key={m.id}>{m.name}</h1></div>)
-                }
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default App;
